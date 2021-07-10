4912987356b36e90d2091529f0e15b7ccffbce0a
yashodgayashan <yashodgayashan@gmail.com>
2021-07-10 08:32:01 +0000

    Added plant defect

---
+++ src/main/java/com/teamgreen/greenhouse/plant-defect/Constants.java
@@ -0,0 +1,15 @@
+package com.teamgreen.greenhouse.plant;
+
+public class Constants {
+
+    private Constants() {}
+
+    public static final String PLANT_TABLE = "plant_disease";
+    public static final String PLANT_ID = "id";
+    public static final String PLANT_PLANT_ID = "plant_id";
+    public static final String PLANT_PLANT_INFO_ID = "plant_info_id";
+    public static final String PLANT_DISEASE_ID = "disease_id";
+    public static final String PLANT_SOLUTION_ID = "solution_id";
+    public static final String PLANT_APPLIED_DATE = "applied_date";
+    public static final String PLANT_RESOLVED_DATE = "resolved_date";
+}
